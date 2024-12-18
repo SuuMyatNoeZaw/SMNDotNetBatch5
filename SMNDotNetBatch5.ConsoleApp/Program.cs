477eefc1cf87f191e2e818405dd9977808d88c95
SuuMyatNoeZaw <suumyatnoezaw.wyb@gmail.com>
2024-12-18 20:26:20 +0300

    feat: add some code

--- SMNDotNetBatch5.ConsoleApp/Program.cs
+++ SMNDotNetBatch5.ConsoleApp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+string connectionString = "Data Source=WINDOWS-1ISKG05\\SQLEXPRESS; Initial Catalog=DotNetTrainingBatch5;Trusted_Connection=True;";
+SqlConnection connection = new SqlConnection(connectionString);
+Console.WriteLine("Connection Opening.....");
+connection.Open();
+string query = @"SELECT[BlogID]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog]";
+
+    SqlCommand cmd =new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt=new DataTable();
+adapter.Fill(dt);
+Console.WriteLine("Connection Closing.....");
+connection.Close();
+
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogID"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
