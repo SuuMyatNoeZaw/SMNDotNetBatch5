0d310d42ee285e1ccfb4a3957c4f7fd4f748e450
SuuMyatNoeZaw <suumyatnoezaw.wyb@gmail.com>
2025-01-04 00:29:14 +0300

    feat:add Database.Model and Console app 2

---
+++ SMNDotNetBatch5.ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SMNDotNetBatch5.Database.Models;
+
+Console.WriteLine("Hello, World!");
+AppDbContext db=new AppDbContext();
+var list=db.TblBlogs.ToList();
