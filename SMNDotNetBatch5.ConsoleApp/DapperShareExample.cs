553b2a69322d40df67e3b711b77ba5461f217c2c
SuuMyatNoeZaw <suumyatnoezaw.wyb@gmail.com>
2025-01-27 21:41:45 +0300

    feat:Add Dapper Service code

---
+++ SMNDotNetBatch5.ConsoleApp/DapperShareExample.cs
@@ -0,0 +1,56 @@
+﻿using SMNDotNetBatch5.ConsoleApp.Models;
+using SMNDotNetBatch5.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMNDotNetBatch5.ConsoleApp
+{
+    public class DapperShareExample
+    {
+        string _connectionString = "Data Source=WINDOWS-1ISKG05\\SQLEXPRESS; Initial Catalog=DotNetTrainingBatch5;Trusted_Connection=True;";
+        private readonly DapperService _dapperService;
+        public DapperShareExample()
+        {
+            _dapperService = new DapperService(_connectionString);
+        }
+        public void Read()
+        {
+
+            string query = "select * from Tbl_Blog where DeleteFlag=0";
+            var lst = _dapperService.Query<BlogDapperDataModel>(query).ToList();
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogID);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+                Console.WriteLine(item.DeleteFlag);
+            }
+        }
+        public void Edit(int id)
+        {
+
+            string query = "select * from Tbl_Blog where DeleteFlag=0 and BlogID=@BlogID";
+            var item = _dapperService.FirstOrDefault<BlogDapperDataModel>(query,new BlogDapperDataModel
+            {
+                BlogID=id
+            });
+            if(item is null)
+            {
+                Console.WriteLine("No Data Found.");
+                return;
+            }
+                Console.WriteLine(item.BlogID);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+                Console.WriteLine(item.DeleteFlag);
+            
+        }
+    }
+    
+}
---
+++ SMNDotNetBatch5.Shared/DapperService.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMNDotNetBatch5.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+
+        }
+        public List<T> Query<T>(string query)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var list=db.Query<T>(query).ToList();
+            return list;
+        }
+        public T FirstOrDefault<T>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var item = db.QueryFirstOrDefault<T>(query,param);
+            return item;
+        }
+    }
+}
