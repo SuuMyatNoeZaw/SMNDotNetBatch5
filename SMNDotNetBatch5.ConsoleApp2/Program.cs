aa09e003d9241535c9fc20032e086981ad78c9d4
SuuMyatNoeZaw <suumyatnoezaw.wyb@gmail.com>
2025-02-03 21:20:20 +0300

    feat: add json example

--- SMNDotNetBatch5.ConsoleApp2/Program.cs
+++ SMNDotNetBatch5.ConsoleApp2/Program.cs
@@ -1,6 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using SMNDotNetBatch5.Database.Models;
 
-Console.WriteLine("Hello, World!");
-AppDbContext db=new AppDbContext();
-var list=db.TblBlogs.ToList();
+//Console.WriteLine("Hello, World!");
+//AppDbContext db=new AppDbContext();
+//var list=db.TblBlogs.ToList();
+var ones = new Jsmodel
+{
+    ID = 1,
+    Name = "Khine Khine",
+    Job = "Teacher",
+
+};
+string jsonStr=JsonConvert.SerializeObject(ones,Formatting.Indented);
+Console.WriteLine(jsonStr);
+public class Jsmodel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Job { get; set; }
+}
