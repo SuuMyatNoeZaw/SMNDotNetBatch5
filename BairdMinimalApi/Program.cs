015c0d0f57820e8b96247a587f01eb1d004d569c
SuuMyatNoeZaw <suumyatnoezaw.wyb@gmail.com>
2025-02-07 20:51:13 +0300

    feat: add  CRUD in birdjson

--- BairdMinimalApi/Program.cs
+++ BairdMinimalApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Newtonsoft.Json;
 using System.Text.Json.Serialization;
@@ -29,16 +30,101 @@ internal class Program
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-       app.MapGet("/birds", () =>
+        app.MapGet("/birds", () =>
+         {
+             string folderPath = "Data/Birds.json";
+             var jsonStr = File.ReadAllText(folderPath);
+             var result = JsonConvert.DeserializeObject<BirdRespondModel>(jsonStr);
+             return Results.Ok(result.Tbl_Bird);
+         })
+         .WithName("BirdGet")
+         .WithOpenApi();
+
+        app.MapGet("/birds/{id}", (int id) =>
         {
             string folderPath = "Data/Birds.json";
             var jsonStr = File.ReadAllText(folderPath);
             var result = JsonConvert.DeserializeObject<BirdRespondModel>(jsonStr);
-            return Results.Ok(result.Tbl_Bird);
+            var item = result.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found.");
+            }
+            return Results.Ok(item);
         })
-        .WithName("BirdGet")
-        .WithOpenApi();
+       .WithName("BirdGetByID")
+       .WithOpenApi();
 
+        app.MapPost("/birds", (BirdModel requestModel) =>
+        {
+            string folderPath = "Data/Birds.json";
+            var jsonStr = File.ReadAllText(folderPath);
+            var result = JsonConvert.DeserializeObject<BirdRespondModel>(jsonStr);
+
+            requestModel.Id = result.Tbl_Bird.Count == 0 ? 1 : result.Tbl_Bird.Max(x => x.Id) + 1;
+            result.Tbl_Bird.Add(requestModel);
+
+            var Ctojson = JsonConvert.SerializeObject(result);
+            File.WriteAllText(folderPath, Ctojson);
+
+            return Results.Ok(requestModel);
+        })
+      .WithName("BirdCreate")
+      .WithOpenApi();
+
+        app.MapPut("/birds/{id}", (int id, BirdModel requestModel) =>
+        {
+            string folderPath = "Data/Birds.json";
+            var jsonStr = File.ReadAllText(folderPath);
+            var result = JsonConvert.DeserializeObject<BirdRespondModel>(jsonStr);
+
+            var item = result.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found.");
+            }
+            var index = result.Tbl_Bird.FindIndex(x => x.Id == id);
+            if (index != -1)
+            {
+                result.Tbl_Bird[index] = new BirdModel
+                {
+                    Id = id,
+                    BirdEnglishName = requestModel.BirdEnglishName,
+                    BirdMyanmarName = requestModel.BirdMyanmarName,
+                    Description = requestModel.Description,
+                    ImagePath = requestModel.ImagePath,
+                };
+
+            }
+
+            var Ctojson = JsonConvert.SerializeObject(result);
+            File.WriteAllText(folderPath, Ctojson);
+
+            return Results.Ok(result.Tbl_Bird[index]);
+        })
+      .WithName("BirdUpdate")
+      .WithOpenApi();
+
+        app.MapDelete("/birds/{id}", (int id) =>
+        {
+            string folderPath = "Data/Birds.json";
+            var jsonStr = File.ReadAllText(folderPath);
+            var result = JsonConvert.DeserializeObject<BirdRespondModel>(jsonStr);
+
+            var item = result.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found.");
+            }
+            result.Tbl_Bird.Remove(item);
+
+            var Ctojson = JsonConvert.SerializeObject(result);
+            File.WriteAllText(folderPath, Ctojson);
+
+            return Results.Ok("Your Data already deleted.");
+        })
+      .WithName("BirdDelete")
+      .WithOpenApi();
         app.Run();
     }
 }
@@ -52,7 +138,7 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
 
 public class BirdRespondModel
 {
-    public BirdModel[] Tbl_Bird { get; set; }//json file name (can't change)
+    public List<BirdModel> Tbl_Bird { get; set; }//json file name (can't change)
 }
 
 public class BirdModel//c# class name
@@ -63,3 +149,14 @@ public class BirdModel//c# class name
     public string Description { get; set; }
     public string ImagePath { get; set; }
 }
+
+
+
+
+
+
+
+
+
+
+
